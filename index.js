14f4e7960d3486d6abc9c1929679964899bca0ba
tungtung99 <tungnguyen99hn1@gmail.com>
2020-10-16 19:59:22 +0700

    updatecode

--- index.js
+++ index.js
@@ -1,43 +1,8 @@
-var check,check1;
-function fromonchange(obj){
-    var value=obj.value;
-    if(value == 'vietnam'){
-        check="vietnam"
-    }
-    else if(value == 'usd'){
-        check="usd"
-    }
-}
-function toonchange(obj){
-    var value=obj.value;
-    if(value == 'vietnam'){
-        check1="vietnam"
-    }
-    else if(value == 'usd'){
-        check1="usd"
-    }
-
-}
 function tinhtoan(){
-    var tienchuyendoi;
-    var tiennhap=document.getElementById("inputtiente").value;
-    var message=document.getElementById("show_message");
-    if(tiennhap == '')
-    {
-        alert("Mời bạn nhập số tiền muốn chuyển đổi");
-    }
-    else{
-        if(check == check1){
-            message.innerHTML = "Result:"+tiennhap;
-        }
-        else{
-            if(check=="vietnam"){
-                tienchuyendoi=tiennhap/23175.70;
-            }
-            else{
-                tienchuyendoi=tiennhap*23175.70;
-            }
-            message.innerHTML = "Result:"+tienchuyendoi;
-        }
-    }
+    let message=document.getElementById("show_message")
+    let tiennhap=document.getElementById("inputtiente").value
+    let tientruockhidoi=document.getElementById("money1").value
+    let tiensaukhidoi=document.getElementById("money2").value
+    let tienxuatra=(tiennhap/tientruockhidoi)*tiensaukhidoi
+    message.innerHTML="Result: "+tienxuatra
 }
